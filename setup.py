b402b473c9d4430f732f6c00cf8522fbf9064be6
Nischay Tayal <tayalnischay@gmail.com>
2022-07-31 15:36:10 +0530

    updated

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
-from setuptools import setup
+from setuptools import setup,find_packages
 from typing import List
 
 # Declaring variables for setup functions
 PROJECT_NAME="housing-predictor"
-VERSION="0.0.1"
+VERSION="0.0.3"
 AUTHOR="Nischay"
 DESCRIPTION="This is first end-to-end machine learning project"
-PACKAGES=["housing"]
+
 REQUIREMENT_FILE_NAME="requirements.txt"
 
 
@@ -19,7 +19,7 @@ def get_requirements_list()->List[str]:
 
     """
     with open(REQUIREMENT_FILE_NAME) as requirement_file:
-        return requirement_file.readlines()
+        return requirement_file.readlines().remove("-e .")
 
 
 setup(
@@ -27,6 +27,6 @@ setup(
     version=VERSION,
     author=AUTHOR,
     description=DESCRIPTION,
-    packages=PACKAGES,
+    packages=find_packages(),
     install_requires=get_requirements_list()
 )
