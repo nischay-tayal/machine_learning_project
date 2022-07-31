bdfcfda0148a31e64baf3d7cff3df82ef9e3a8c8
Nischay Tayal <tayalnischay@gmail.com>
2022-07-31 14:49:24 +0530

    updated

--- README.md
+++ README.md
@@ -62,9 +62,9 @@ git remote -v
 ```
 
 To setup CI/CD pipeline in heroku we need 3 information
-1. HEROKU_EMAIL = anishyadav7045075175@gmail.com
+1. HEROKU_EMAIL = tayalnischay@gmail.com
 2. HEROKU_API_KEY = <>
-3. HEROKU_APP_NAME = ml-regression-app
+3. HEROKU_APP_NAME = ml--regression--app
 
 BUILD DOCKER IMAGE
 ```
@@ -91,4 +91,10 @@ docker ps
 Tos stop docker conatiner
 ```
 docker stop <container_id>
+```
+
+
+
+```
+python setup.py install
 ```
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+from typing import List
+
+# Declaring variables for setup functions
+PROJECT_NAME="housing-predictor"
+VERSION="0.0.1"
+AUTHOR="Nischay"
+DESCRIPTION="This is first end-to-end machine learning project"
+PACKAGES=["housing"]
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+
+def get_requirements_list()->List[str]:
+    """
+    Description: This function is going to return list of requiremnet 
+    mention in requirements.txt file 
+
+    return This fuction is going to return a list which contain name of libraries mentioned in requirements.txt file
+
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        return requirement_file.readlines()
+
+
+setup(
+    name=PROJECT_NAME,
+    version=VERSION,
+    author=AUTHOR,
+    description=DESCRIPTION,
+    packages=PACKAGES,
+    install_requires=get_requirements_list()
+)
